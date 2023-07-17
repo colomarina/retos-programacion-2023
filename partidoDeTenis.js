e88d2f9d6ef519bedf220b1e635975e05ab5f353
Lucas Marina <72533178+colomarina@users.noreply.github.com>
2023-07-17 16:28:30 -0300

    feat: hice el segundo reto
    https://github.com/mouredev/retos-programacion-2023

---
+++ partidoDeTenis.js
@@ -0,0 +1,48 @@
+/*
+ * Escribe un programa que muestre cómo transcurre un juego de tenis y quién lo ha ganado.
+ * El programa recibirá una secuencia formada por "P1" (Player 1) o "P2" (Player 2), según quien
+ * gane cada punto del juego.
+ * 
+ * - Las puntuaciones de un juego son "Love" (cero), 15, 30, 40, "Deuce" (empate), ventaja.
+ * - Ante la secuencia [P1, P1, P2, P2, P1, P2, P1, P1], el programa mostraría lo siguiente:
+ *   15 - Love
+ *   30 - Love
+ *   30 - 15
+ *   30 - 30
+ *   40 - 30
+ *   Deuce
+ *   Ventaja P1
+ *   Ha ganado el P1
+ * - Si quieres, puedes controlar errores en la entrada de datos.   
+ * - Consulta las reglas del juego si tienes dudas sobre el sistema de puntos.   
+ */
+
+const namePoints = ['Love', 15, 30, 40]
+
+const winner = (array) => {
+  let pointsP1 = 0;
+  let pointsP2 = 0;
+  array.forEach(player => {
+    if (player === "P1") {
+      pointsP1 += 1
+    } else {
+      pointsP2 += 1
+    }
+    if (pointsP1 === 5 || pointsP2 === 5) { 
+      return console.log(`Ha ganado ${player}`)
+    }
+    if (pointsP1 === 3 && pointsP2 === 3) {
+      console.log(`Deuce`)
+    } else  
+    if (pointsP1 === 4) {
+      console.log(`Ventaja ${player}`)
+    } else
+    if (pointsP2 === 4) {
+      console.log(`Ventaja ${player}`)
+    } else {
+      console.log(`${namePoints[pointsP1]} - ${namePoints[pointsP2]}`)
+    }
+  });
+}
+
+winner(["P1", "P1", "P2", "P2", "P1", "P2", "P1", "P1"])
